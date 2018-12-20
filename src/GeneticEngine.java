e4fe42b49bb4421bec03e4b0681bb475674ba285
Kirk Reynolds [AU Sydney] [Avante] <Kirk.Reynolds@avante-group.com>
2018-12-21 10:49:43 +1100

    Beginning to flesh out files

--- src/GeneticEngine.java
+++ src/GeneticEngine.java
@@ -11,5 +11,7 @@
  * 
  */
 public class GeneticEngine {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,7 +1,23 @@
 /*
  * Single worker unit for control of testing a single genome
  * Class in charge of reproduction and fitness testing.
+ * 
+ * Unsure if should only be for calculating one tick or if each worker
+ * should have various multipliers like a throttle and gradient mutliplier
+ * and use those to simulate a whole flight
  */
 public class Worker {
-
+	
+	Worker() {
+		
+	}
+	private void testFitness() {
+		//Tests fitness and passes up to manager
+	}
+	private void simulate() {
+		//Called by testFitness to run step by step calculation
+	}
+	private void physicsCalculations() {
+		//Runs all the physical calculations and passes it up to simulate()
+	}
 }
