28a6ef1b0e0c5165c044131d67a95d253600770f
Kirk Reynolds [AU Sydney] [Avante] <Kirk.Reynolds@avante-group.com>
2018-12-21 17:36:40 +1100

    Finishing for the day

--- src/Manager.java
+++ src/Manager.java
@@ -3,5 +3,25 @@
  * threads and passing fitness results to the tasker
  */
 public class Manager {
-	
+	Manager() {
+		
+	}
+	public void createPool() {
+		//Initialise the task pool
+	}
+	private void addTasks() {
+		//Add tasks to the pool
+	}
+	private void sendResults() {
+		//Send top genome to tasker for analysis
+	}
+	private void breedGenome() {
+		//Breed the genomes after the weak are killed off
+	}
+	private void killGenomes() {
+		//Take test results from analyze genomes and kill the weak
+	}
+	private void analyseGenomes() {
+		//Test genome fitness
+	}
 }
