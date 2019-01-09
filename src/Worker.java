2eb53c6a58a279aeffe432d04d66e38ce53a31f7
Kirk Reynolds <kirk.reynolds@protonmail.com>
2019-01-09 15:57:29 +1100

    Work work

--- src/Worker.java
+++ src/Worker.java
@@ -6,19 +6,26 @@
  * should have various multipliers like a throttle and gradient mutliplier
  * and use those to simulate a whole flight
  */
-public class Worker {
-	
-	Worker() {
-		
+public class Worker implements Runnable {
+	private int Genome = null;
+	private int Goal = null;
+	Worker(String Genome, String Goal) {
+		this.Genome = Genome;
+		this.Goal = Goal;
+	}
+	public void run() {
+		process();
 	}
 	private void testFitness() {
 		//Tests fitness and passes up to manager
 	}
 	private void simulate() {
 		//Called by testFitness to run step by step calculation
+		//For now a nothing function, just guessing numbers
 	}
 	private void physicsCalculations() {
 		//Runs all the physical calculations and passes it up to simulate()
+		//For now a nothing function, just guessing numbers
 	}
 	void process() {
 		//Call other methods and pass out to the manager
